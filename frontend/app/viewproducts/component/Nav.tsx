eec3e4ceb9d18aee838a7fc5fcdc9d60d8cd863b
Thinley <thinleylama44@gmail.com>
2024-05-10 12:23:04 +0530

    orders button in navbar

--- frontend/app/viewproducts/component/Nav.tsx
+++ frontend/app/viewproducts/component/Nav.tsx
@@ -4,9 +4,14 @@ import Logo from '@/app/viewproducts/component/assets/Logo.png'
 
 function Nav() {
     return (
-        <div className=" bg-white">
-            <div className="flex justify-start items-center h-14 p-2 ">
+        <div className=" bg-slate-200 flex justify-between h-16 items-center">
+            <div className="p-1 ml-2 bg-white rounded-md">
                 <Image src={Logo} alt='logo' className='w-36 cursor-pointer' />
+            </div>
+            <div className='mr-2 '>
+                <Link href={'/order'}>
+                    <button type="button" className="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Orders</button>
+                </Link>
             </div>
         </div>
     )
