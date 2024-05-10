1d3ab7148be8113ee31a51f1b57f6d4b493f4e90
Thinley <thinleylama44@gmail.com>
2024-05-10 12:31:07 +0530

    orders button in navbar

--- frontend/app/viewproducts/component/Nav.tsx
+++ frontend/app/viewproducts/component/Nav.tsx
@@ -8,9 +8,9 @@ function Nav() {
             <div className="p-1 ml-2 bg-white rounded-md">
                 <Image src={Logo} alt='logo' className='w-36 cursor-pointer' />
             </div>
-            <div className='mr-2 '>
+            <div className=''>
                 <Link href={'/order'}>
-                    <button type="button" className="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Orders</button>
+                    <button type="button" className="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Orders</button>
                 </Link>
             </div>
         </div>
